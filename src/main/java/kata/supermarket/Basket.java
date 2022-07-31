55f6f0c59b1ebfbdb3b3fac66b77fb71e67315d2
Christos Ioannou <khrisdoes@gmail.com>
2022-07-31 20:38:46 +0100

    Add basket discount and buy one get one free implementation

--- src/main/java/kata/supermarket/Basket.java
+++ src/main/java/kata/supermarket/Basket.java
@@ -2,9 +2,7 @@ package kata.supermarket;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Basket {
     private final List<Item> items;
@@ -39,19 +37,61 @@ public class Basket {
                     .setScale(2, RoundingMode.HALF_UP);
         }
 
-        /**
-         * TODO: This could be a good place to apply the results of
-         *  the discount calculations.
-         *  It is not likely to be the best place to do those calculations.
-         *  Think about how Basket could interact with something
-         *  which provides that functionality.
-         */
-        private BigDecimal discounts() {
-            return BigDecimal.ZERO;
+        private BigDecimal calculate() {
+            return subtotal().subtract(new DiscountCalculator().discounts());
+        }
+    }
+
+    private class DiscountCalculator {
+
+        private Set<Discount> activeDiscounts;
+
+        private DiscountCalculator() {
+            this.activeDiscounts = new HashSet<>(
+                    Collections.singletonList(
+                            new BuyOneGetOneFreeSingleItemDiscount(null)
+                    )
+            );
         }
 
-        private BigDecimal calculate() {
-            return subtotal().subtract(discounts());
+        private BigDecimal discounts() {
+            return activeDiscounts.stream().map(Discount::calculate).
+                    reduce(BigDecimal::add)
+                    .orElse(BigDecimal.ZERO)
+                    .setScale(2, RoundingMode.HALF_UP);
+        }
+    }
+
+    private interface Discount {
+        BigDecimal calculate();
+    }
+
+    private abstract class SingleItemDiscount implements Discount {
+
+        private Item discountedItem;
+
+        public SingleItemDiscount(Item discountedItem) {
+            this.discountedItem = discountedItem;
+        }
+    }
+
+    private class BuyOneGetOneFreeSingleItemDiscount extends SingleItemDiscount {
+
+        public BuyOneGetOneFreeSingleItemDiscount(Item discountedItem) {
+            super(discountedItem);
+        }
+
+        @Override
+        public BigDecimal calculate() {
+            Set<BigDecimal> seenItemPrice = new HashSet<>();
+            for (Item item : items) {
+                // Find seen item by price - assuming no different items have the same price
+                if (seenItemPrice.contains(item.price())) {
+                    return item.price();
+                }
+                seenItemPrice.add(item.price());
+            }
+            return BigDecimal.ZERO;
         }
     }
 }
